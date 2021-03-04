b42fae14898cf2f37a39fb95962f6f92f6e8d915
tarek doha <tarikdoha80@gmail.com>
2021-03-04 23:09:40 +0100

    direct About US to SiteTabNames.MetaData

--- Models/Pages/AboutUsPageType.cs
+++ Models/Pages/AboutUsPageType.cs
@@ -2,6 +2,7 @@
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 using EPiServer.SpecializedProperties;
+using EpiServer_Setup.Business;
 using EpiServer_Setup.Models.Pages.Base;
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -16,7 +17,7 @@ namespace EpiServer_Setup.Models.Pages
                 [Display(
                     Name = "Main body",
                     Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
-                    GroupName = SystemTabNames.Content,
+                    GroupName = SiteTabNames.Metadata,
                     Order = 1)]
                 public virtual XhtmlString MainBody { get; set; }
          
